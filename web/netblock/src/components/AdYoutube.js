f1c8b4cd0565404971f4876d95c9c29e47756a25
Two-Weeks <webmon@kakao.com>
2021-10-08 16:15:03 +0900

    🔨 refactor: modal size

--- web/netblock/src/components/AdYoutube.js
+++ web/netblock/src/components/AdYoutube.js
@@ -4,8 +4,6 @@ import YouTube from "react-youtube";
 class AdYoutube extends React.Component {
   render() {
     const opts = {
-      height: "300",
-      width: "400",
       playerVars: {
         // https://developers.google.com/youtube/player_parameters
         autoplay: 1,
