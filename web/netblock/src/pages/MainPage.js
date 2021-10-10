641462bb3cf7c6b5990b7ee5ff81931b73e35e3e
Two-Weeks <webmon@kakao.com>
2021-10-10 23:29:19 +0900

    ➕ feat: Link main download button to download Page

--- web/netblock/src/pages/MainPage.js
+++ web/netblock/src/pages/MainPage.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Contents from "../components/Contents";
 import MainDownloadButton from "../components/MainDownloadButton";
+import { Link } from "react-router-dom";
 
 const MainPage = () => {
   return (
     <div className="mainPage">
       <Contents></Contents>
-      <MainDownloadButton />
+      <Link to="/download">
+        <MainDownloadButton />
+      </Link>
     </div>
   );
 };
